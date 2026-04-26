1beef5f585072f814e84051b04f90d782419a4ac
Kalani <kalanihaack@gmail.com>
2026-04-26 18:59:55 -0300

    C05 - Criando uma funcao via arrow function para o display atualizar a data e hora e ajeitando a estrutura de funcoes

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,35 +1,71 @@
 class calcController {
 
     constructor() {
-        this._displayCalc = "0"
+
+        this._displayCalcEl = document.querySelector("#display")
+        this._dateEl = document.querySelector("#data")
+        this._timeEl = document.querySelector("#hora")
         this._currentDate
         this.initialize()
+        this._locale = 'pt-br' //variavel para informar o pais que sera puxado os dados
     }
 
     initialize() {
 
-        let displayCalcEl = document.querySelector("#display")
-        let dateEl = document.querySelector("#data")
-        let timeEl = document.querySelector("#hora")
+        this.setDisplayTime()
 
-        displayCalcEl.innerHTML = "teste"
-        dateEl.innerHTML = "teste"
-        timeEl.innerHTML = "teste"
+        setInterval(() => { //arrowFunction na pratica
+
+            this.setDisplayTime()
+
+        }, 1000) //funcao para atualizar a data e hora a cada 1000ms
+
+    }
+
+    setDisplayTime(){
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+                day: "2-digit",
+                month: "long",
+                year: "numeric"
+            })
+
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+
     }
 
     get displayCalc() {
-        return this._displayCalc
+        return this._displayCalcEl.innerHTML
     }
 
     set displayCalc(value) {
-        this._displayCalc = value
+        this._displayCalcEl.innerHTML = value
     }
 
     get currentDate() {
-        return this._currentDate
+        return new Date()
     }
 
     set currentDate(value) {
         this._currentDate = value
     }
+
+    get displayTime() {
+        return this._timeEl.innerHTML
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value
+    }
 }
+
+
+//lendo a documentacao do ecmascript2022, percebi que agora metodos privados podem ser chamados usando #, ao inves de _
