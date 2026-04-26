abb467873f19b759bb39ab2cf14849b09abfca19
Kalani <kalanihaack@gmail.com>
2026-04-26 11:55:48 -0300

    C04 - Adicionando o metodo initialize para alterar os dados da calculadora via DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -3,6 +3,18 @@ class calcController {
     constructor() {
         this._displayCalc = "0"
         this._currentDate
+        this.initialize()
+    }
+
+    initialize() {
+
+        let displayCalcEl = document.querySelector("#display")
+        let dateEl = document.querySelector("#data")
+        let timeEl = document.querySelector("#hora")
+
+        displayCalcEl.innerHTML = "teste"
+        dateEl.innerHTML = "teste"
+        timeEl.innerHTML = "teste"
     }
 
     get displayCalc() {
