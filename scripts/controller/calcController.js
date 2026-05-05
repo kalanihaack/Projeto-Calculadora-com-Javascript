6a32a5b69a2b9a6a937b7e8aeb170f2cf0f5eebb
Kalani <kalanihaack@gmail.com>
2026-05-05 20:48:42 -0300

    C21 - Adicionando um try catch no eval para retorno de operacoes indevidas

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -185,8 +185,13 @@ class calcController {
     }
 
     getResult(){
-
-        return eval(this._operation.join(""))
+        try{
+            return eval(this._operation.join(""))
+        } catch(e){
+            setTimeout(()=>{
+                this.setError()
+            }, 1)
+        }
     }
 
     calc(){
